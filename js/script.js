4f5988f6a617842729b03cc0a462ff0885a87cde
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-11-14 14:30:11 -0500

    Sync

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Ashley Monaghan All rights reserved
 //
 // Created by: Ashley Monaghan
-// Created on: Oct 2022
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,31 +10,21 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2OR-Unit-5-01/sw.js", {
+    scope: "/ICS2OR-Unit-5-01/",
   })
 }
-
 /**
- * This function displays an alert.
- * Math
+ * This function updates the slider value.
  */
-function calculate() {
-  // input
-  const a = parseInt(document.getElementById("textbox-a").value)
-  const b = parseInt(document.getElementById("textbox-b").value)
-  const c = parseInt(document.getElementById("textbox-c").value)
-
-  // process
-  const math = a + b + c
-  // output
-  document.getElementById("math").innerHTML = "Numbers: " + math + " units"
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
 }
 
 /**
- * This function displays an alert.
- * Words
+ * This function displays the slider value.
  */
-function button() {
-  document.getElementById("words").innerHTML = "<p>Hello, World!</p>"
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML =
+    "<p>Value is: " + slider.value + "</p>"
 }
